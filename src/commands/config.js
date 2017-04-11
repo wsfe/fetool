8c02b5c2bfd7534fd05947120a7f4560961b01cd
angrytoro <angrytoro@gmail.com>
2017-04-11 22:38:23 +0800

    添加config

---
+++ src/commands/config.js
@@ -0,0 +1,16 @@
+
+export default function config(program) {
+  program.command('config [key] [value]')
+    .description('全局配置')
+    .action((key, value) => {
+      if (!key || !value) { // 如果没有带参数，默认显示所有配置
+        // todo 查看配置
+        return;
+      }
+      if (key && value) { // 设置全局变量
+        
+      } else { // 提示命令错误
+        error('fet config <[key] [value]\n');
+      }
+    });
+};
