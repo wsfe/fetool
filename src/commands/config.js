1bcc0cde9a0a8eaf708a8e51dd3b6681f7bfd023
angrytoro <angrytoro@gmail.com>
2017-04-12 07:26:01 +0800

    1.完成config全局配置命令 2.删除老旧的config.bak.js

--- src/commands/config.js
+++ src/commands/config.js
@@ -1,16 +1,21 @@
+import rightPad from 'right-pad';
 
 export default function config(program) {
   program.command('config [key] [value]')
     .description('全局配置')
     .action((key, value) => {
+      let globalConfig = JSON.parse(fs.readFileSync(FET_RC, { encoding: 'utf8' }));
       if (!key || !value) { // 如果没有带参数，默认显示所有配置
-        // todo 查看配置
+        Object.keys(globalConfig).forEach((k) => {
+          log(`${rightPad(k, 6)} = ${globalConfig[k]}`);
+        });
         return;
       }
       if (key && value) { // 设置全局变量
-        
+        globalConfig[key] = value;
+        fs.writeFileSync(FET_RC, JSON.stringify(globalConfig, null, '    '), 'UTF-8');
       } else { // 提示命令错误
-        error('fet config <[key] [value]\n');
+        error('fet config [key] [value]\n');
       }
     });
 };
