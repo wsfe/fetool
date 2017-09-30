a1b413958ee5143120c87ffb971599f09fac9341
angrytoro <angrytoro@gmail.com>
2017-09-30 11:43:05 +0800

    删除单页应用的css配置，让项目更加灵活配置

--- src/models/single.config.js
+++ src/models/single.config.js
@@ -6,30 +6,7 @@ import ExtractTextPlugin from 'extract-text-webpack-plugin';
 class SingleConfig extends Config {
   constructor(project) {
     super(project);
-    this.setDefaultModuleRules();
     this.setWebpackConfig(this.extendConfig.webpackConfig);
-  }
-
-  setDefaultModuleRules() {
-    this.baseConfig.module.rules = _.concat(this.baseConfig.module.rules, [{
-      test: /\.css$/,
-      use: ExtractTextPlugin.extract({
-        fallback: require.resolve('style-loader'),
-        use: require.resolve('css-loader')
-      })
-    }, {
-      test: /\.less$/,
-      use: ExtractTextPlugin.extract({
-        fallback: require.resolve('style-loader'),
-        use: [require.resolve('css-loader'), require.resolve('less-loader')]
-      })
-    }, {
-      test: /\.(scss|sass)$/,
-      use: ExtractTextPlugin.extract({
-        fallback: require.resolve('style-loader'),
-        use: [require.resolve('css-loader'), require.resolve('sass-loader')]
-      })
-    }])
   }
 
   setWebpackConfig(webpackConfig = {}) {
@@ -44,24 +21,6 @@ class SingleConfig extends Config {
     this.fixContext(this.baseConfig);
     this.fixAlias(this.baseConfig);
     this.fixOutput(this.baseConfig);
-    this.fixPlugins();
-  }
-
-  fixPlugins() {
-    let isExitExtractTextPlugin = this.baseConfig.plugins.some((plugin) => { // 判断是否是整个项目共用一个ExtractTextPlugin
-      return plugin instanceof ExtractTextPlugin;
-    });
-    if (!isExitExtractTextPlugin) {
-      let filename = 'base@[contenthash].css';
-      if (this.NODE_ENV === ENV.LOC) {
-        filename = 'base.css';
-      } else if (this.NODE_ENV === ENV.DEV){
-        filename = 'base@dev.css';
-      }
-      this.baseConfig.plugins.push(new ExtractTextPlugin({
-        filename
-      }));
-    }
   }
 }
 
