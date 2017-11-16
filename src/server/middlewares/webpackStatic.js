b2b8dae6dadf4965ce5904514845541ba48ff2d7
angrytoro <angrytoro@gmail.com>
2017-11-16 18:31:48 +0800

    修复多页应用获取图片，字体等静态资源的时候，出现定位错误的问题。

--- src/server/middlewares/webpackStatic.js
+++ src/server/middlewares/webpackStatic.js
@@ -1,7 +1,7 @@
 import { projectService } from '../../services'
 
 const STATIC_REG = /.*\.(png|jpe?g|gif|svg|woff2?|eot|ttf|otf)$/
-const VER_REG = /@[\d\w]+(?=\.\w+)/;
+const VER_REG = /[@|\.|_|#|\-][\d\w]+(?=\.\w+$)/; // 静态文件文件名，添加hash的时候，支持用艾特(@),点号(.),井号(#),下划线(_),以及中横线（-）分割,例如：icons.1212121323.ttf或者icons@1212121323.ttf等
 
 export default (req, res, next) => {
   let filePaths = req.url.split('/')
@@ -9,7 +9,7 @@ export default (req, res, next) => {
     let projectName = filePaths[1],
       project = projectService.getDevProject(sysPath.join(process.cwd(), projectName))
     if (project.mode === MUTLI_MODE) {
-      filePaths.splice(2)
+      filePaths.splice(2, 1)
       req.url = filePaths.join('/').replace(VER_REG, '')
     }
   }
