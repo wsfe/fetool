361790751259f59a3e5deb0ed4502f75a2a328b8
angrytoro <angrytoro@gmail.com>
2017-11-16 21:40:19 +0800

    1. 多页应用中，图片字体加载问题。2. 删除没有必要的依赖包，减低安装时间

--- src/server/middlewares/webpackStatic.js
+++ src/server/middlewares/webpackStatic.js
@@ -1,7 +1,7 @@
 import { projectService } from '../../services'
 
 const STATIC_REG = /.*\.(png|jpe?g|gif|svg|woff2?|eot|ttf|otf)$/
-const VER_REG = /[@|\.|_|#|\-][\d\w]+(?=\.\w+$)/; // 静态文件文件名，添加hash的时候，支持用艾特(@),点号(.),井号(#),下划线(_),以及中横线（-）分割,例如：icons.1212121323.ttf或者icons@1212121323.ttf等
+const VER_REG = /[\W][\d\w]+(?=\.\w+$)/;
 
 export default (req, res, next) => {
   let filePaths = req.url.split('/')
