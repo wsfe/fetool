203f23258a5f20aa8a5905b7717d8dbfaa221072
angrytoro <angrytoro@gmail.com>
2017-11-24 16:05:18 +0800

    1.修复压缩插件打包产生的bug，2.升级lint命令，把原先自己写的格式化换成eslint-friendly-formatter

--- src/commands/lint.js
+++ src/commands/lint.js
@@ -1,68 +1,9 @@
 import standard from 'standard';
-import chalk from 'chalk';
-import table from 'text-table';
+// import chalk from 'chalk';
+// import table from 'text-table';
+// import stripAnsi from 'strip-ansi'
+import formatter from 'eslint-friendly-formatter'
 import Project from '../models/project';
-
-/**
- * Given a word and a count, append an s if count is not one.
- * @param {string} word A word in its singular form.
- * @param {int} count A number controlling whether word should be pluralized.
- * @returns {string} The original word with an s on the end if count is not one.
- */
-function pluralize (word, count) {
-  return (count === 1 ? word : word + 's');
-}
-
-function processResults(results) {
-  let output = '\n';
-  let total = 0;
-
-  results.forEach(function (result) {
-    let messages = result.messages;
-
-    if (messages.length === 0) {
-      return;
-    }
-
-    total += messages.length;
-    output += chalk.underline(result.filePath) + '\n';
-
-    output += table(
-      messages.map(function (message) {
-        let messageType;
-
-        messageType = chalk.red('error');
-
-        return [
-          '',
-          message.line || 0,
-          message.column || 0,
-          messageType,
-          message.message.replace(/\.$/, ''),
-          chalk.dim(message.ruleId || '')
-        ];
-      }),
-      {
-        align: ['', 'r', 'l'],
-        stringLength: function (str) {
-          return chalk.stripColor(str).length;
-        }
-      }
-    ).split('\n').map(function (el) {
-      return el.replace(/(\d+)\s+(\d+)/, function (m, p1, p2) {
-        return chalk.dim(p1 + ':' + p2);
-      });
-    }).join('\n') + '\n\n';
-  });
-
-  if (total > 0) {
-    output += chalk.red.bold([
-      '\u2716 ', total, pluralize(' problem', total), '\n'
-    ].join(''));
-  }
-
-  return total > 0 ? output : '';
-}
 
 export default function lint(program) {
   program.command('lint')
@@ -79,9 +20,9 @@ export default function lint(program) {
           error(err);
           process.exit(1);
         }
-        let parseResult = processResults(results.results);
+        let parseResult = formatter(results.results);
         if (parseResult) {
-          console.log();
+          console.log(parseResult);
         } else {
           success('lint success');
         }
