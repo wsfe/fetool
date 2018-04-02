9561b12dd679dac97af512fc8bf7b1be474658d4
angrytoro <angrytoro@gmail.com>
2018-04-02 17:56:24 +0800

    修复转代理的时候，没有删除hash值问题

--- src/server/middlewares/hashReplacer.js
+++ src/server/middlewares/hashReplacer.js
@@ -12,11 +12,11 @@ const VER_REG = /@[\d\w]+(?=\.\w+)/
 export default function replaceHash(req, res, next) {
   let url = req.url.replace(QUERY_REG, '')
   if (/.*\.(js|css)$/.test(url)) {
-    req.url = url
-    const filePaths = req.url.split('/')
+    const filePaths = url.split('/')
     // 如果url == '/projectname/prd/..../xxx@hash值.js|css'，那么把这些hash都删除掉。
     if (filePaths[2] === OUTPUT_DIR) {
-      req.url = req.url.replace(QUERY_REG, '').replace(VER_REG, '')
+      req.url = url.replace(QUERY_REG, '').replace(VER_REG, '')
+      req.originalUrl = req.url
     }
   }
   next()
