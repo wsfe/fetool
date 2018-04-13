3b447af7fe826db6df640a0f69c5983cc90bf24a
zhangxiaoyun <zhangxiaoyun0828@sina.cn>
2018-04-13 18:31:07 +0800

    调试好了模板下载

--- src/commands/init.js
+++ src/commands/init.js
@@ -12,8 +12,8 @@ const getGitUser = require('../utils/git-user')
 const ask = require('../utils/ask')
 const filter = require('../utils/filter')
 const exists = fs.existsSync
-const templatePath = 'vue-template/webpack'
-const tmpPath = path.join(home, '.wsfe-templates/webpack')
+const templatePath = 'wsfe/vue-template'
+const tmpPath = path.join(home, '.wsfe-templates/single')
 
 export default function init (program) {
   /**
@@ -30,7 +30,7 @@ export default function init (program) {
       const inPlace = !rawName || rawName === '.'
       const projectName = inPlace ? path.relative('../', process.cwd()) : rawName
       const to = path.resolve(projectName || '.')
-      const clone = program.clone
+      const clone = cmd.clone
 
       /**
        * Padding.
@@ -63,22 +63,24 @@ export default function init (program) {
        * download and generator project.
        */
       function run () {
-        const spinner = ora('download template')
+        const spinner = ora('download template...')
         spinner.start()
         // Remove if local template exists
         if (exists(tmpPath)) rm(tmpPath)
         download(templatePath, tmpPath, { clone }, err => {
           if (err) {
-            error('download failed' + err)
+            spinner.stop()
+            error('download failed')
             process.exit(1)
           }
+          spinner.succeed()
           generate(projectName, tmpPath, to, err => {
             if (err) {
               error(err)
               process.exit(1)
             }
             console.log()
-            success('Generated')
+            success('Generated success')
           })
         })
       }
@@ -88,7 +90,7 @@ export default function init (program) {
        */
       function generate (name, src, dest, done) {
         const opts = getOptions(name)
-        const metalsmith = Metalsmith(path.join(src, 'template'))
+        const metalsmith = Metalsmith(src)
         const data = Object.assign(metalsmith.metadata(), {
           destDirName: name,
           inPlace: dest === process.cwd(),
@@ -96,9 +98,10 @@ export default function init (program) {
         })
 
         metalsmith
-          .use(askQuestions(opts.prompts))
-          .user(filterFiles(opts.filters))
+          // .use(askQuestions(opts.prompts))
+          // .use(filterFiles(opts.filters))
           .clean(false)
+          .source('.')
           .destination(dest)
           .build((err, files) => {
             done(err)
@@ -169,35 +172,4 @@ export default function init (program) {
         return sortedObject
       }
     })
-
-  // program.command('init [type]')
-  //   .description('初始化项目,可以根据type来制定是vue还是ts')
-  //   .action((type) => {
-  //     let isExit = true,
-  //       filePath = sysPath.join(process.cwd(), 'ft.config.js');
-  //     try {
-  //       isExit = fs.statSync(filePath).isFile();
-  //     } catch (err) {
-  //       isExit = false;
-  //     }
-
-  //     if (!isExit) {
-  //       try {
-  //         fs.readFile(sysPath.join(__dirname, '../config', type ? `${type}.ft.config.js` : 'ft.config.js'), { encoding: 'utf8' }, (err, data) => {
-  //           if (err) {
-  //             error(err);
-  //           } else {
-  //             fs.writeFileSync(filePath, data, 'UTF-8');
-  //             success('init success');
-  //           }
-  //           process.exit();
-  //         });
-  //       } catch (err) {
-  //         error(err);
-  //         process.exit();
-  //       }
-  //     } else {
-  //       process.exit();
-  //     }
-  //   });
 }
