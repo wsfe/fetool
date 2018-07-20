315b11c1f15508d071c8940580b486be66650bfb
angrytoro <angrytoro@gmail.com>
2018-07-20 18:41:18 +0800

    新增样式hash替换插件

---
+++ src/plugins/replaceCssHash.js
@@ -0,0 +1,47 @@
+import crypto from 'crypto'
+import _ from 'lodash'
+
+const HASH_REG = /@\w+/g
+
+function md5(content) {
+  return crypto.createHash('md5').update(content).digest('hex')
+}
+
+class ReplaceCssHash {
+  constructor() {}
+
+  apply (compiler) {
+    compiler.plugin("emit", (compilation, callback) => {
+      let needRemoveAssets = []
+      let assets = compilation.assets
+      compilation.chunks.forEach(chunk => {
+        let filenames = []
+        chunk.files.forEach(filename => {
+          if (sysPath.extname(filename) === '.css') {
+            let file = compilation.assets[filename]
+            let hash = md5(file.source())
+            let renderedHash = hash.slice(0, 20)
+            let newFilename = filename.replace(HASH_REG, () => {
+              return `@${renderedHash}`
+            })
+            assets[newFilename] = file
+            filenames.push(newFilename)
+            chunk.hash = hash
+            chunk.renderedHash = renderedHash
+            needRemoveAssets.push(filename)
+          }
+        })
+        chunk.files = filenames
+      })
+      assets = _.reduce(assets, (result, value, key) => {
+        if (needRemoveAssets.indexOf(key) < 0) {
+          result[key] = value
+        }
+        return result
+      }, {})
+      compilation.assets = assets
+    })
+  }
+}
+
+export default ReplaceCssHash
